700994ea3d7a703df2e0066b5e3af30410655791
dangqhuy <dangqhuy2411@g.gmail.com>
2018-10-08 17:34:41 +0700

    check repo exist

--- fabfile.py
+++ fabfile.py
@@ -7,12 +7,16 @@ env.hosts = ['18.222.128.254']
 env.user = 'ubuntu'
 
 def deloy():
-    run("cd var/www ; git clone {}".format(REPO))
-    run("curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py")
-    sudo("python2 get-pip.py")
-    sudo("apt-get install python-flask")
-    sudo("python2 -m pip install psycopg2")
-    sudo("python2 -m pip install flask-bcrypt")
-    sudo("python2 -m pip install gevent")
-    sudo("python2 -m pip install flask-wtf")
-    run("cd var/www/practice-flask/ ; export FLASK_APP=hello.py ; python -m flask run")
+    if os.path.isdir("/var/www/practice-flask"):
+        sudo("cd /var/www/pratice-flask ; git pull origin master")
+        run("cd /var/www/practice-flask/ ; export FLASK_APP=hello.py ; python -m flask run")
+    else:
+        sudo("cd /var/www ; git clone {}".format(REPO))
+        run("curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py")
+        sudo("python2 get-pip.py")
+        sudo("apt-get install python-flask")
+        sudo("python2 -m pip install psycopg2")
+        sudo("python2 -m pip install flask-bcrypt")
+        sudo("python2 -m pip install gevent")
+        sudo("python2 -m pip install flask-wtf")
+        run("cd /var/www/practice-flask/ ; export FLASK_APP=hello.py ; python -m flask run")
